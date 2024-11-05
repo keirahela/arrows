5224739c3aa0838ea008e296513ee2075c056beb
Kósa Márk Milán <kosa.mark@verebely.dc>
2024-11-05 14:10:06 +0100

    2. ora

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
 let gameRunning = false;
 let currentMove = null;
+let currentInfo = { Correct: 0, Incorrect: 0 };
 
 function generateNewMove() {
     let _move = possibleTexts[getRndInteger(0, possibleTexts.length - 1)].toLowerCase()
@@ -15,15 +18,32 @@ function generateNewMove() {
     console.log(currentMove);
 }
 
+async function showInfo(currentArrow, isValid) {
+    document.getElementById(currentArrow).style.backgroundColor = isValid ? "lightgreen" : "lightcoral";
+    await sleep(200);
+    document.getElementById(currentArrow).style.backgroundColor = "rgb(228, 228, 228)";
+    currentInfo[isValid ? "Correct" : "Incorrect"] += 1
+}
+
+function showResults() {
+    document.getElementById('randomtext').innerText = `${currentInfo["Correct"]}/${currentInfo["Correct"] + currentInfo["Incorrect"]}. Avg: ${(currentInfo["Correct"] / (currentInfo["Correct"] + currentInfo["Incorrect"])).toFixed(3)}%`
+}
+
 
 document.addEventListener("DOMContentLoaded", (event) => {
-    document.onkeydown = function (e) {
+    document.onkeydown = async function (e) {
         e = e || window.Event;
         // use e.keyCode
         console.log(currentMove)
         console.log(e.key)
-        if(currentMove == e.key) {
-            generateNewMove();
+        if(possibleMoves.includes(e.key)) {
+            if(currentMove == e.key) {
+                await showInfo(e.key, true);
+                generateNewMove();
+            } else {
+                await showInfo(e.key, false);
+                generateNewMove();
+            }
         }
     };
 
@@ -34,8 +54,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
 
     document.getElementById('stop').onclick = () => {
-        if(gameRunning) return;
-        gameRunning = true;
-        generateNewMove();
+        if(!gameRunning) return;
+        showResults();
+        gameRunning = false
     }
 });
